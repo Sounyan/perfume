6161fb6f37d8c620d3e816ddec7cb8e2e0518cf7
Sounyan <69900395+Sounyan@users.noreply.github.com>
2020-08-21 14:33:14 +0900

    あ〜

--- main.js
+++ main.js
@@ -43,6 +43,19 @@ server.listen(port, function() {
   console.log("ok");
 });
 
+const URL = 'https://perfume-bot.herokuapp.com/'; // 独自ドメインでもOK
+const INTERVAL_MSEC = 10 * 60 * 1000; // 10分毎(30分未満ならOK)
+
+setInterval(() => {
+  http
+    .get(URL, res => {
+      console.log(res, URL);
+    })
+    .on('error', err => {
+      console.log(err, URL);
+    });
+}, INTERVAL_MSEC);
+
 // Discord bot implements
 const discord = require("discord.js");
 const client = new discord.Client();
